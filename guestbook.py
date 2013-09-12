673b7ada75c87f11f5ce3da6021a014a5b231781
Isaac Lean <isaaclean@creatibyte.com>
2013-09-12 04:35:15 -0700

    Playing around with form handling

--- guestbook.py
+++ guestbook.py
@@ -1,16 +1,29 @@
+import cgi
 from google.appengine.api import users
 import webapp2
 
+MAIN_PAGE_HTML = """\
+<html>
+    <body>
+        <form action="/sign" method="post">
+        <div><textarea name="content" rows="3" cols="60"></textarea></div>
+        <div><input type="submit" value="Sign Guestbook"></div>
+        </form>
+    </body>
+</html>
+"""
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-        
-        if user:
-            self.response.headers["Content-Type"] = "text/plain"
-            self.response.write("Welcome " + user.nickname())
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+        self.response.write(MAIN_PAGE_HTML)
+
+class Guestbook(webapp2.RequestHandler):
+    def post(self):
+        self.response.write("<html><body>You wrote:<pre>")
+        self.response.write(cgi.escape(self.request.get("content")))
+        self.response.write("</pre></body></html>")
 
 application = webapp2.WSGIApplication([
-                                       ('/', MainPage),
+    ('/', MainPage),
+    ("/sign", Guestbook),
 ], debug=True)
