7ab77e63c45484f23e00863d84bbaac523afaaff
Isaac Lean <isaaclean@creatibyte.com>
2013-09-11 23:50:03 -0700

    *Playing around with the Users service part of the SDK *Edited ".gitignore" to ignore .pyc files

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,15 @@
+from google.appengine.api import users
 import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers["Content-Type"] = "text/plain"
-        self.response.write("Hello world!")
+        user = users.get_current_user()
+        
+        if user:
+            self.response.headers["Content-Type"] = "text/plain"
+            self.response.write("Welcome " + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
 
 application = webapp2.WSGIApplication([
                                        ('/', MainPage),
