36386b3fa30a676dbb76f98212920b95617e86ac
Isaac Lean <isaaclean@creatibyte.com>
2013-09-17 01:03:33 -0700

    Started working on a very basic version of the financial management part of the app

--- owl.py
+++ owl.py
@@ -1,62 +1,46 @@
 import os
-import urllib
-import jinja2
 import webapp2
+import jinja2
 
-from google.appengine.api import users
-from google.appengine.ext import ndb
+from google.appengine.ext import db
 
-JINJA_ENVIRONMENT = jinja2.Environment(
-                                       loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
-                                       extensions = ["jinja2.ext.autoescape"])
-DEFAULT_GUESTBOOK_NAME = "default_guestbook"
+TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), "templates")
+JINJA_ENV = jinja2.Environment(loader = jinja2.FileSystemLoader(TEMPLATE_DIR),
+                                       autoescape = True)
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+        
+    def render_str(self, template, **params):
+        t = JINJA_ENV.get_template(template)
+        return t.render(params)
+    
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
 
-def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
-    return ndb.Key("Guestbook", guestbook_name)
-
-class Greeting(ndb.Model):
-    author = ndb.UserProperty()
-    content = ndb.StringProperty(indexed=False)
-    date = ndb.DateTimeProperty(auto_now_add=True)
-
-class MainPage(webapp2.RequestHandler):
+class MainPage(Handler):
+    def render_front(self, date = "", description = "", business = "", category = "", transtype = "", amount = "",
+                     error = ""):
+        self.render("front.html", date = date, description = description, business = business, category = category,
+                    transtype = transtype, amount = amount, error = error)
+    
     def get(self):
-        guestbook_name = self.request.get("guestbook_name", DEFAULT_GUESTBOOK_NAME)
-        greetings_query = Greeting.query(ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
-        greetings = greetings_query.fetch(10)
-            
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = "Logout"
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = "Login"
-                
-        template_values = {
-            "greetings": greetings,
-            "guestbook_name": urllib.quote_plus(guestbook_name),
-            "url": url,
-            "url_linktext": url_linktext
-        }
-            
-        template = JINJA_ENVIRONMENT.get_template("index.html")
-        self.response.write(template.render(template_values))
+        self.render_front()
         
-class Guestbook(webapp2.RequestHandler):
     def post(self):
-        guestbook_name = self.request.get("guestbook_name", DEFAULT_GUESTBOOK_NAME)
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
+        date = self.request.get("date")
+        description = self.request.get("description")
+        business = self.request.get("business")
+        category = self.request.get("category")
+        transtype = self.request.get("transtype")
+        amount = self.request.get("amount")
         
-        if users.get_current_user():
-            greeting.author = users.get_current_user()
-            
-        greeting.content = self.request.get("content")
-        greeting.put()
-        
-        query_params = {"guestbook_name": guestbook_name}
-        self.redirect("/?" + urllib.urlencode(query_params))
+        if date and amount:
+            self.write("Input successful!")
+        else:
+            error = "You must input a date and amount!"
+            self.render_front(date, description, business, category, transtype, amount, error)
 
-application = webapp2.WSGIApplication([
+app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ("/sign", Guestbook),
 ], debug=True)
